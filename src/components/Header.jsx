74a0a16387899c3fb321aa0120f63e5f14f759a7
Sangram-Deshmukh <sangramdeshmukh3435@gmail.com>
2025-03-04 00:01:52 +0530

    tried adding bg image to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import logoImg from '../assets/logo1.png';
 
 export default function Header() {
   return (
-    <header>
+    <header class="bg-[url(/src/assets/BackgroundImg.jpg)] ...">
       <img src={logoImg} alt="A form and a pencil" />
       <h1>Contact Form</h1>
+
+
     </header>
   );
 }
